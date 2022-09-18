683abf3bdaf62b1f3903b50f2d2ad74d69a63ec7
CalebeSG <calebeguimaraes10@gmail.com>
2022-09-18 09:51:49 -0300

    layout responsive

--- web/src/components/CreateAdBanner.tsx
+++ web/src/components/CreateAdBanner.tsx
@@ -3,13 +3,13 @@ import * as Dialog from '@radix-ui/react-dialog'
 
 export function CreateAdBanner() {
   return (
-    <div className="pt-1 bg-nlw-gradient mt-8 w-full rounded-lg overflow-hidden">
-      <div className="bg-[#2a2634] px-8 py-6 rounded-t-lg flex justify-between items-center">
+    <div className="pt-1 bg-nlw-gradient mt-8 w-full rounded-t-3xl md:rounded-lg overflow-hidden">
+      <div className="bg-[#2a2634] px-8 py-10 md:py-6 rounded-t-3xl md:rounded-t-lg flex flex-col md:flex-row md:justify-between text-center md:text-left items-center gap-10">
         <div>
           <strong className="text-2xl text-white font-black block">
             Não encontrou seu duo?
           </strong>
-          <span className="text-zinc-400">
+          <span className="text-zinc-400 block mt-4 md:mt-0">
             Publique um anúncio para encontrar novos players!
           </span>
         </div>
